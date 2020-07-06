a4432ca579aac21f7577830a1b0279f5bd6a9499
hinapupil <hinapupil@gmail.com>
2020-07-07 05:17:29 +0900

    first commit

--- sql.js
+++ sql.js
@@ -1,4 +1,4 @@
-const sqlite3 = require('sqlite3').verbose();
+	const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test.db');
 
 if( process.argv.length < 3 )	process.exit(0);
